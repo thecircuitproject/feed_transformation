e1242c92acf6fdd742766f3d15628262b8bfb491
Jose Carlos <thecircuitproject@gmail.com>
2024-03-10 20:52:05 -0600

    Refactored the code to use functions instead of a class
    - Removed the class definition and converted all methods to standalone functions
    - Updated the imports and function calls accordingly
    - This change makes the code more modular and easier to package

--- feedtransformation.py
+++ feedtransformation.py
@@ -1,244 +1,209 @@
-import pandas as pd
 import polars as pl
-import numpy as np
-from typing import List, Union, Self
+from typing import List, Union
 from polars import ColumnNotFoundError
 import warnings
 
 
-class FeedTransformation:
+def rename_cols(feed: pl.DataFrame, rename_dict: dict) -> pl.DataFrame:
     """
-    This class is used to transform a feed.
+    Rename columns of the feed
 
-    To visualize the dataframe, call the feed attribute after each method.
+    Parameters:
+        rename_dict (dict): A dictionary with the columns to rename
 
-    Attributes:
-        feed (pl.DataFrame): A polars DataFrame
-        current_metadata (str): The name of the current metadata column
-
-    Methods:
-        rename_cols(rename_dict:dict): Rename columns of the feed
-        format_cols(): Format columns of the feed
-        filter_products(col:str, filter_dict:dict): Filter products of the feed
-        create_metadata(cols:list,col_name:str='metadata'): Create metadata column
-        all_combinations_metadata(cols:list, on_col:Union[str, List[str]], col_name:str='metadata'): Create metadata column for all combinations of on_col
-        group_metadata(group_cols:list, order:bool=False): Group metadata column
-        rename_column_value(col:str, old:str, new:str, regex=False, ow:str=None): Rename column value
-        export_json(path:str): Export feed to json
-        export_csv(path:str): Export feed to csv
+    Returns:
+        self
     """
+    if not isinstance(rename_dict, dict):
+        raise TypeError("rename_dict must be a dictionary")
 
-    def __init__(self, feed: pl.DataFrame) -> None:
-        """
-        Parameters:
-            feed (pl.DataFrame): A polars DataFrame
+    return feed.rename(rename_dict)
 
-        Returns:
-            None
-        """
 
-        if not isinstance(feed, pl.DataFrame):
-            raise TypeError("Feed must be a polars DataFrame")
+def format_cols(feed: pl.DataFrame) -> pl.DataFrame:
+    """
+    Format columns of the feed
 
-        self.feed = feed
-        self.current_metadata = None
+    Parameters:
+        None
 
-    def rename_cols(self, rename_dict: dict) -> Self:
-        """
-        Rename columns of the feed
+    Returns:
+        self
+    """
+    return feed.select(
+        pl.all().map_alias(lambda col: col.strip().lower().replace(" ", "_"))
+    )
 
-        Parameters:
-            rename_dict (dict): A dictionary with the columns to rename
 
-        Returns:
-            self
-        """
-        if not isinstance(rename_dict, dict):
-            raise TypeError("rename_dict must be a dictionary")
+def filter_products(feed: pl.DataFrame, col: str, filter_dict: dict):
+    pass
 
-        self.feed = self.feed.rename(rename_dict)
-        return self
 
-    def format_cols(self) -> Self:
-        """
-        Format columns of the feed
+def create_metadata(
+    feed: pl.DataFrame, cols: list, meta_name: str = "metadata"
+) -> pl.DataFrame:
+    """
+    Create metadata column as a struct of the columns in cols
 
-        Parameters:
-            None
+    Parameters:
+        cols (list): A list of columns to be included in the metadata column
+        meta_name (str): The name of the metadata column
 
-        Returns:
-            self
-        """
-        self.feed = self.feed.select(
-            pl.all().map_alias(lambda col: col.strip().lower().replace(" ", "_"))
+    Returns:
+        self
+    """
+    _validate_existing_columns(feed, cols)
+
+    _overwrite_metadata(feed, meta_name)
+    return feed.with_columns(pl.struct(cols).alias(meta_name))
+
+
+def all_combinations_metadata(
+    feed: pl.DataFrame,
+    cols: list,
+    on_col: Union[str, List[str]],
+    meta_name: str = "metadata",
+) -> pl.DataFrame:
+    """
+    Create metadata column as a struct of the columns in cols for all combinations of on_col
+
+    Parameters:
+        cols (list): A list of columns to be included in the metadata column
+        on_col (str or list): The column(s) to group by
+        col_name (str): The name of the metadata column
+
+    Returns:
+        self
+    """
+    _validate_existing_columns(feed, cols)
+
+    _overwrite_metadata(feed, meta_name)
+
+    current_metadata = meta_name
+    comb_metadata = (
+        feed.with_columns(pl.struct(cols).alias(meta_name))
+        .select(pl.col(meta_name, on_col))
+        .group_by(on_col)
+        .agg(pl.col(meta_name))
+    )
+    return feed.join(comb_metadata, on=on_col, how="left")
+
+
+def group_metadata(
+    feed: pl.DataFrame,
+    group_cols: Union[List[str], str],
+    metadata: str = "metadata",
+    order: bool = False,
+) -> pl.DataFrame:
+    """
+    Group metadata column by group_cols and keep the first value of the other columns
+
+    Parameters:
+        group_cols (list or str): A list of columns to group by (or a single column name)
+        order (bool): Whether to maintain the order of the feed
+
+    Returns:
+        self
+    """
+    try:
+        feed.select(pl.col(group_cols))
+    except ColumnNotFoundError:
+        raise ColumnNotFoundError("One or more columns in cols not found in feed")
+
+    cols = [col for col in feed.columns if col not in group_cols and col != metadata]
+    return feed.group_by(group_cols, maintain_order=order).agg(
+        pl.col(cols).first(), pl.col(metadata)
+    )
+
+
+def rename_column_value(
+    feed: pl.DataFrame, col: str, old: str, new: str, regex=False, ow: str = None
+) -> pl.DataFrame:
+    """
+    Rename column value from old to new in column col
+
+    Parameters:
+        col (str): The name of the column
+        old (str): The old value
+        new (str): The new value
+        regex (bool): Whether to use regex
+        ow (str): The value to replace with if old is not found
+
+    Returns:
+        self
+    """
+    _validate_existing_columns(feed, col)
+
+    if ow is None:
+        ow = old
+        warnings.warn(f"ow not specified. Using old value {old} as ow")
+
+    if regex:
+        feed = feed.with_columns(pl.col(col).str.replace(rf"{old}", new))
+    else:
+        feed = feed.with_columns(
+            pl.when(pl.col(col) == old)
+            .then(pl.lit(new))
+            .otherwise(pl.lit(ow))
+            .alias(col)
         )
-        return self
+    return feed
 
-    def filter_products(self, col: str, filter_dict: dict):
-        pass
 
-    def create_metadata(self, cols: list, meta_name: str = "metadata") -> Self:
-        """
-        Create metadata column as a struct of the columns in cols
+def export_json(
+    feed: pl.DataFrame, path: str, finalize: bool = True
+) -> pl.DataFrame | None:
+    """
+    Export feed to json
 
-        Parameters:
-            cols (list): A list of columns to be included in the metadata column
-            meta_name (str): The name of the metadata column
+    Parameters:
+        path (str): The path to export the feed
+        finalize (bool): Whether to finalize the feed (i.e. return None)
+    Returns:
+        self
+    """
+    feed.write_json(path, row_oriented=True, pretty=True)
 
-        Returns:
-            self
-        """
+    if finalize:
+        return None
+
+    return feed
+
+
+def export_csv(
+    feed: pl.DataFrame, path: str, finalize: bool = False
+) -> pl.DataFrame | None:
+    """
+    Export feed to csv
+
+    Parameters:
+        path (str): The path to export the feed
+        finalize (bool): Whether to finalize the feed (i.e. return None)
+
+    Returns:
+        self
+    """
+    feed.write_csv(path)
+    if finalize:
+        return None
+    return feed
+
+
+def _validate_existing_columns(df: pl.DataFrame, cols: list[str] | str) -> None:
+    if isinstance(cols, list):
         try:
-            self.feed.select(pl.col(cols))
+            df.select(pl.col(cols))
         except ColumnNotFoundError:
-            raise ColumnNotFoundError("One or more columns in cols not found in feed")
-
-        if meta_name in self.feed.columns:
-            warnings.warn(
-                f"Column {meta_name} already exists in feed. It will be overwritten"
-            )
-
-        self.feed = self.feed.with_columns(pl.struct(cols).alias(meta_name))
-
-        if self.current_metadata:
-            self.feed = self.feed.drop(self.current_metadata)
-        self.current_metadata = meta_name
-        return self
-
-    def all_combinations_metadata(
-        self, cols: list, on_col: Union[str, List[str]], meta_name: str = "metadata"
-    ) -> Self:
-        """
-        Create metadata column as a struct of the columns in cols for all combinations of on_col
-
-        Parameters:
-            cols (list): A list of columns to be included in the metadata column
-            on_col (str or list): The column(s) to group by
-            col_name (str): The name of the metadata column
-
-        Returns:
-            self
-        """
+            raise ColumnNotFoundError("One or more columns in cols not found in feed.")
+    else:
         try:
-            self.feed.select(pl.col(cols))
+            df.select(pl.col(cols))
         except ColumnNotFoundError:
-            raise ColumnNotFoundError("One or more columns in cols not found in feed")
+            raise ColumnNotFoundError(f"Column {cols} not found in feed")
 
-        if meta_name in self.feed.columns:
-            warnings.warn(
-                f"Column {meta_name} already exists in feed. It will be overwritten"
-            )
 
-        self.current_metadata = meta_name
-        comb_metadata = (
-            self.feed.with_columns(pl.struct(cols).alias(meta_name))
-            .select(pl.col(meta_name, on_col))
-            .groupby(on_col)
-            .agg(pl.col(meta_name))
+def _overwrite_metadata(df: pl.DataFrame, metadata: str) -> None:
+    if metadata in df.columns:
+        warnings.warn(
+            f"Column {metadata} already exists in feed. It will be overwritten"
         )
-        self.feed = self.feed.join(comb_metadata, on=on_col, how="left")
-
-        if self.current_metadata:
-            self.feed = self.feed.drop(self.current_metadata)
-        self.current_metadata = meta_name
-        return self
-
-    def group_metadata(
-        self, group_cols: Union[List[str], str], order: bool = False
-    ) -> Self:
-        """
-        Group metadata column by group_cols and keep the first value of the other columns
-
-        Parameters:
-            group_cols (list or str): A list of columns to group by (or a single column name)
-            order (bool): Whether to maintain the order of the feed
-
-        Returns:
-            self
-        """
-        try:
-            self.feed.select(pl.col(group_cols))
-        except ColumnNotFoundError:
-            raise ColumnNotFoundError("One or more columns in cols not found in feed")
-
-        if self.current_metadata is None:
-            raise ValueError("Metadata column not found. Use create_metadata() first")
-
-        cols = [
-            col
-            for col in self.feed.columns
-            if col not in group_cols and col != self.current_metadata
-        ]
-        self.feed = self.feed.groupby(group_cols, maintain_order=order).agg(
-            pl.col(cols).first(), pl.col(self.current_metadata)
-        )
-        return self
-
-    def rename_column_value(
-        self, col: str, old: str, new: str, regex=False, ow: str = None
-    ) -> Self:
-        """
-        Rename column value from old to new in column col
-
-        Parameters:
-            col (str): The name of the column
-            old (str): The old value
-            new (str): The new value
-            regex (bool): Whether to use regex
-            ow (str): The value to replace with if old is not found
-
-        Returns:
-            self
-        """
-        try:
-            self.feed.select(pl.col(col))
-        except ColumnNotFoundError:
-            raise ColumnNotFoundError(f"Column {col} not found in feed")
-
-        if ow is None:
-            ow = old
-            warnings.warn(f"ow not specified. Using old value {old} as ow")
-
-        if regex:
-            self.feed = self.feed.with_columns(pl.col(col).str.replace(rf"{old}", new))
-        else:
-            self.feed = self.feed.with_columns(
-                pl.when(pl.col(col) == old)
-                .then(pl.lit(new))
-                .otherwise(pl.lit(ow))
-                .alias(col)
-            )
-        return self
-
-    def export_json(self, path: str, finalize: bool = True) -> Self | None:
-        """
-        Export feed to json
-
-        Parameters:
-            path (str): The path to export the feed
-            finalize (bool): Whether to finalize the feed (i.e. return None)
-        Returns:
-            self
-        """
-        self.feed.write_json(path, row_oriented=True, pretty=True)
-
-        if finalize:
-            return None
-
-        return self
-
-    def export_csv(self, path: str, finalize: bool = False) -> Self | None:
-        """
-        Export feed to csv
-
-        Parameters:
-            path (str): The path to export the feed
-            finalize (bool): Whether to finalize the feed (i.e. return None)
-
-        Returns:
-            self
-        """
-        self.feed.write_csv(path)
-        if finalize:
-            return None
-        return self
