a4110ff2a166e4cf0d8c4d2409c1f4ab0b7b50ad
Jose Carlos <thecircuitproject@gmail.com>
2024-03-15 11:49:22 -0600

    Uploaded the first version of the Python package for creating nested JSON feed files using Polars

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="feedtransformation",
+    version="0.0.1",
+    description="A Python package that leverages the power of Polars to efficiently group and create nested JSON product feed files. Ideal for handling large datasets and complex JSON structures.",
+    author="José Carlos Borrayo Tojín",
+    author_email="thecircuitproject1@gmail.com",
+    packages=find_packages(),
+    install_requires=["polars>=0.20.14"],
+)
